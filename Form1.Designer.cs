50df1e7d0a8da8e0b302b3d7e052f2b5a74a03b0
ianli0623 <gogoing0623@gmail.com>
2022-10-27 14:03:22 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace testDBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_con = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_con
+            // 
+            this.txt_con.Location = new System.Drawing.Point(12, 39);
+            this.txt_con.Name = "txt_con";
+            this.txt_con.Size = new System.Drawing.Size(997, 25);
+            this.txt_con.TabIndex = 0;
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(920, 82);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(89, 34);
+            this.OK.TabIndex = 1;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 135);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.txt_con);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_con;
+        private System.Windows.Forms.Button OK;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testDBConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txt_con.Text = "Data Source=localhost;Initial Catalog=; Persist Security Info=True;User ID=sa;Password=";
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(txt_con.Text);
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    MessageBox.Show("success");
+                }
+                else
+                {
+                    MessageBox.Show("fail!");
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
